784b7938f98a279fc32cd81dd6a8f888ea391a6f
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-10-28 23:35:56 -0500

    Solved twoSum problem and recorded results

--- twoSum.js
+++ twoSum.js
@@ -18,9 +18,24 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-var nums = [2, 7, 11, 15]
-var target = 9; // Expected output: [0,1]
-
 var twoSum = function(nums, target) {
-    
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if ((nums[i] + nums[j]) == target) {
+                // Change all instances of console.log() to return when submitting to Leetcode
+                console.log([i, j]);
+            }
+        }
+    }
 };
+
+twoSum([2, 7, 11, 15], 9); // Returns [0,1]
+twoSum([3, 2, 4], 6); // Returns [1,2]
+twoSum([3,3], 6); // Returns [0,1]
+twoSum([2, 6, 7, 8, 10, 12, 14], 19) // Running this test case just in case, returns [2,5]
+
+/* 
+Runtime: 94 ms, beating only 26.64% of JavaScript users
+Memory: 41.72 MB, beating 94.94% of JavaScript users
+Overall: slow but a light workload
+*/
