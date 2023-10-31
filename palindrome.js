b4b81f28ac5abdc3cfa8079958fddf68b5429375
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-10-30 23:46:00 -0500

    Correctly solved palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -19,5 +19,31 @@
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
 var isPalindrome = function(x) {
-    
+    // Convert the integer to 2 identical strings first
+    var xString = x.toString();
+    var yString = "";
+
+    // Add characters from xString to yString so that yString is the reverse of xString for comparison
+    for (i = xString.length - 1; i > -1; i--) {
+        yString = yString + xString[i];
+    };
+
+    // Run the comparison
+    if (xString == yString) {
+        // Change all instances of console.log() to return when submitting to Leetcode
+        console.log(true);
+    } else {
+        console.log(false);
+    };
 };
+
+isPalindrome(121); // Returns true
+isPalindrome(456); // Returns false
+isPalindrome(-303); // Returns false since "-" is the first character, not 3
+isPalindrome(1000021); // Returns false
+
+/* 
+Runtime: 149 ms, beating only 35.14% of JavaScript users
+Memory: 51.14 MB, beating only 45.79% of JavaScript users
+Overall: slow and a heavy workload, but it works
+*/
